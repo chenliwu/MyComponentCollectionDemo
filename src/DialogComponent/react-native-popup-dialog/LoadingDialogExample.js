025ec46d26d579185cf4b7d430a206fab01a25c3
chenlw <chenlw@bytter.com>
2018-12-05 15:42:24 +0800

    封装加载对话框全局样式

--- src/DialogComponent/react-native-popup-dialog/LoadingDialogExample.js
+++ src/DialogComponent/react-native-popup-dialog/LoadingDialogExample.js
@@ -14,6 +14,8 @@ import Dialog, {
     ScaleAnimation,
 } from 'react-native-popup-dialog';
 
+import GlobalLoadingDialogStyle from './GlobalLoadingDialogStyle';
+
 export default class LoadingDialogExample extends React.Component {
 
     static navigationOptions = {
@@ -51,29 +53,23 @@ export default class LoadingDialogExample extends React.Component {
                     onDismiss={() => {
                         this.setState({defaultAnimationDialog: false});
                     }}
-                    width={0.4}
-                    height={0.15}
+                    width={GlobalLoadingDialogStyle.dialogWidthPercent}
+                    height={GlobalLoadingDialogStyle.dialogHeightPercent}
                     visible={this.state.defaultAnimationDialog}
                     rounded
-                    containerStyle={{   //设置dialog容器样式，这个容器布满整个屏幕
-                        backgroundColor: '#00000000'
-                    }}
-                    overlayBackgroundColor='#00000000'  //设置遮盖层的背景色
-                    dialogStyle={{
-                        backgroundColor: 'rgba(0,0,0,0.8)',
-                    }}
+                    // containerStyle={{   //设置dialog容器样式，这个容器布满整个屏幕
+                    //     backgroundColor: '#00000000'
+                    // }}
+                    overlayBackgroundColor={GlobalLoadingDialogStyle.overlayBackgroundColor}  //设置遮盖层的背景色
+                    dialogStyle={GlobalLoadingDialogStyle.dialogStyle}
                     //dialogAnimation={new ScaleAnimation()}  //动画效果
                     // onHardwareBackPress={()=>{
                     //     return false;
                     // }}
                 >
-                    <View style={{
-                        flex: 1,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}>
+                    <View style={GlobalLoadingDialogStyle.dialogContentContainerStyle}>
                         <ActivityIndicator size="large"/>
-                        <Text style={{color: '#fff', marginTop: 10}}>加载中...</Text>
+                        <Text style={GlobalLoadingDialogStyle.loadingHintTextStyle}>加载中...</Text>
                     </View>
                 </Dialog>
 
