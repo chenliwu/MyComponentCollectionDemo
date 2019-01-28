1c3df5c94c4bdf0c1f822e8a47d7c0029d914bbd
chenlw <chenlw@bytter.com>
2019-01-28 16:25:06 +0800

    网络状态监听

--- src/react-native-api/NetInfo/NetInfoExample.js
+++ src/react-native-api/NetInfo/NetInfoExample.js
@@ -17,14 +17,21 @@ import {
  * NetInfo API可以获取设备联网或者离线的状态信息。
  *
 
- 1、当设备断开网络时：{type: "none", effectiveType: "unknown"}
- 2、当设备连接wifi时：{type: "wifi", effectiveType: "unknown"}
- 3、当设备连接移动网络时：{type: "mobile", effectiveType: "unknown"}
+ connectionInfo.type有如下跨平台可用的值:
+
+ none - 设备处于离线状态
+ wifi - 设备通过wifi联网，或者设备是iOS模拟器
+ cellular - 设备通过蜂窝数据流量联网
+ unknown - 联网状态异常
 
  *
  *
  */
 export default class NetInfoExample extends Component {
+
+    static navigationOptions = {
+        headerTitle: 'NetInfo'
+    };
 
     constructor(props) {
         super(props);
@@ -35,40 +42,48 @@ export default class NetInfoExample extends Component {
 
     componentDidMount() {
         //监听网络的变化
-        NetInfo.addEventListener('connectionChange', this._handleConnectionInfoChange);
-        NetInfo.fetch().done((connectionInfo) => {
-            console.log('fetch.done');
-            console.log(connectionInfo);
-            this.setState({
-                connectionInfo: connectionInfo
-            });
-        });
+        NetInfo.addEventListener(
+            'connectionChange',
+            this._handleFirstConnectivityChange
+        );
     }
 
-    _handleConnectionInfoChange = (connectionInfo) => {
-        console.log('connectionChange');
+    /**
+     * 监听网络状态的变化
+     *
+     * 1、IOS
+     * （1）
+     *
+     * @param connectionInfo
+     * @private
+     */
+    _handleFirstConnectivityChange = (connectionInfo) => {
+        console.log('_handleFirstConnectivityChange');
         console.log(connectionInfo);
+        console.log('First change, type: ' + connectionInfo.type + ', effectiveType: ' + connectionInfo.effectiveType);
         this.setState({
-            connectionInfo: connectionInfo
+            connectionInfo: 'First change, type: ' + connectionInfo.type + ', effectiveType: ' + connectionInfo.effectiveType
         });
     };
 
     render() {
         return (
             <View style={styles.container}>
-                <Text>是否联网：{}</Text>
-                <Button title={'是否联网'} onPress={()=>{
-                    console.log(NetInfo.isConnected);
+                <Text>联网状态：{this.state.connectionInfo}</Text>
+                <Button title={'是否联网'} onPress={() => {
                     NetInfo.isConnected.fetch().done((isConnected) => {
                         console.log('First, is ' + (isConnected ? 'online' : 'offline'));
+                        console.log(isConnected);
+                        alert('First, is ' + (isConnected ? 'online' : 'offline'));
                     });
                 }}/>
             </View>
         )
     }
 
-    componentWillUnmount(){
-        NetInfo.removeEventListener('connectionChange', this._handleConnectionInfoChange);
+    componentWillUnmount() {
+        //移除网络监听
+        NetInfo.removeEventListener('connectionChange', this._handleFirstConnectivityChange);
     }
 
 }
