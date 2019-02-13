434219c9b1b432a774d16af79b352c81f6703fd0
chenlw <chenlw@bytter.com>
2019-02-13 09:34:47 +0800

    创建react-native-camera分支

--- src/AppDraweNavigator.js
+++ src/AppDraweNavigator.js
@@ -5,28 +5,7 @@ import {
     Button
 } from 'react-native';
 
-///滑动删除组件 2018-12-5
-import SwipeComponentNavigator from './swipeComponent/SwipeComponentNavigator';
 
-///手风琴
-import AccordionComponentNavigator from './AccordionComponent/AccordionComponentNavigator';
-
-//对话框组件
-import DialogsComponentNavigator from './DialogComponent/DialogsComponentNavigator';
-
-//Toast组件
-import ToastNavigator from './Toast/ToastNavigator';
-
-//Modal模态框 2018-12-6
-import ModalComponentNavigator from './ModalComponent/ModalComponentNavigator';
-
-
-//动画 2018-12-10
-
-
-
-//键盘事件 2018-12-12
-import KeyboardExample from './Keyboard/KeyboardExample';
 
 import {
     SafeAreaView,
@@ -71,48 +50,6 @@ export default createDrawerNavigator(
             screen: AppNavigatorPage,
             navigationOptions:{
                 drawerLabel: "首页",
-            }
-        },
-        SwipeComponentNavigator: {
-            screen: SwipeComponentNavigator,
-            navigationOptions:{
-                drawerLabel: '侧滑组件',
-            }
-        },
-        AccordionComponentNavigator: {
-            screen: AccordionComponentNavigator,
-            navigationOptions:{
-                drawerLabel: '手风琴',
-            }
-        },
-
-        //DialogsComponentNavigator
-        DialogsComponentNavigator: {
-            screen: DialogsComponentNavigator,
-            navigationOptions:{
-                drawerLabel: '对话框',
-            }
-        },
-
-        ToastNavigator: {
-            screen: ToastNavigator,
-            navigationOptions:{
-                drawerLabel: 'Toast提示',
-            }
-        },
-
-        ModalComponentNavigator: {
-            screen: ModalComponentNavigator,
-            navigationOptions:{
-                drawerLabel: 'Modal模态框',
-            }
-        },
-
-
-        KeyboardExample: {
-            screen: KeyboardExample,
-            navigationOptions:{
-                drawerLabel: '键盘事件',
             }
         },
 
